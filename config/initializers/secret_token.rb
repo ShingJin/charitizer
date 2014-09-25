edf4903360b3bca49b64bc86ff6ec6b2e675ca3c
jinxin <238357@gmail.com>
2014-09-26 05:19:47 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Charitizer::Application.config.secret_key_base = '9da9634d7b4c8e10dfa5b0556f98b17cd40411b7f698fa7c29f791d3af3990d7984f048fd8863576416bb3f735a299da314d76fca182e808f845b63a43fb02ea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Charitizer::Application.config.session_store :cookie_store, key: '_charitizer_session'
