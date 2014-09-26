d2829e4ca11276bff7084087cae5b33fafbe2167
jinxin <238357@gmail.com>
2014-09-26 12:10:53 +0800

    shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,14 +1,12 @@
-class Shop < ActiveRecord::Base
-	 def self.store(session)
-    	shop = Shop.new(domain: session.url, token: session.token)
-    	shop.save!
-    	shop.id
-  	 end
+ class Shop < ActiveRecord::Base
+   def self.store(session)
+     shop = Shop.new(domain: session.url, token: session.token)
+     shop.save!
+     shop.id
+   end
 
-	def self.retrieve(id)
-	  return if id.blank?
-	  shop = Shop.find(id)
-	  ShopifyAPI::Session.new(shop.domain, shop.token)
-	end
-
+   def retrieve(id)
+     shop = Shop.find(id)
+     ShopifyAPI::Session.new(shop.domain, shop.token)
+   end
 end
