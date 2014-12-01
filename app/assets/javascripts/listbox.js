011647f2ffb8731c4c3868e1389d881564d4bf0f
jinxin <238357@gmail.com>
2014-12-02 03:58:19 +0800

    ss

--- app/assets/javascripts/listbox.js
+++ app/assets/javascripts/listbox.js
@@ -34,8 +34,8 @@ function assignListCol()
 			$('#collection_second_list').append('<option selected="selected" value="'+$(this).attr('name')+'">'+selected.text+'</option>');
 		}
 		if($("#collection_second_list  option[value='"+selected.value+"']").length>1){
-		//$("#collection_second_list  option[value='"+selected.value+"']").remove();
-	}
+			$("#collection_second_list  option[value='"+selected.value+"']").remove();
+		}
 
 
 	});
