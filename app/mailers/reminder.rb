29db65e91a0e3dcfa87943e963b72bf403410fd0
jinxin <238357@gmail.com>
2014-12-03 10:21:41 +0800

    ddwer" "

--- app/mailers/reminder.rb
+++ app/mailers/reminder.rb
@@ -6,7 +6,7 @@ class Reminder < ActionMailer::Base
   	@rules = Rule.where("paid =?", false)
 
   	for rule in @rules 
-     if (Time.now.to_date - rule.created_at.to_date)/(1.day) == 30
+     if ((Time.now.to_date - rule.created_at.to_date) / (1.day)).to_i == 30
       rule.notified = true
       rule.save
   		reminder_email(rule)
