34610a03c6edaa85c58b6559f6b3686133450022
jinxin <238357@gmail.com>
2014-12-06 09:21:24 +0800

    ss

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -310,9 +310,10 @@ class RulesController < ApplicationController
 
       end
 
-      
+
         for o in @orders#@rule.first_created < o.created_at.to_datetime
-          if (@rule.first_created < Time.parse(o.updated_at) && @rule.permanent == true ) || (o.cancelled_at.nil? && o.created_at.to_datetime > @rule.starting_date && o.created_at.to_datetime < @rule.ending_date)
+          time = o.created.to_datetime
+          if (o.cancelled_at.nil? && @rule.permanent == true ) || (o.cancelled_at.nil? && o.created_at.to_datetime > @rule.starting_date && o.created_at.to_datetime < @rule.ending_date)
             if @rule.per_order!=nil
               @amount = @amount + (@rule.by_percentage ? (@rule.per_order.to_f/100)*o.total_line_items_price.to_f : @rule.per_order.to_f )
             else
