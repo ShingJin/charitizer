85388b3214eef06192ece933c5ee266f7b1a8228
jinxin <238357@gmail.com>
2014-12-06 08:14:10 +0800

    error

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -313,8 +313,8 @@ class RulesController < ApplicationController
 
 
       
-        for o in @orders
-          if (o.cancelled_at.nil? && @rule.permanent == true && @rule.first_created < o.created_at.to_datetime ) || (o.cancelled_at.nil? && o.created_at.to_datetime > @rule.starting_date && o.created_at.to_datetime < @rule.ending_date)
+        for o in @orders#@rule.first_created < o.created_at.to_datetime
+          if (o.cancelled_at.nil? && @rule.permanent == true ) || (o.cancelled_at.nil? && o.created_at.to_datetime > @rule.starting_date && o.created_at.to_datetime < @rule.ending_date)
             if @rule.per_order!=nil
               @amount = @amount + (@rule.by_percentage ? (@rule.per_order.to_f/100)*o.total_line_items_price.to_f : @rule.per_order.to_f )
             else
